572f613a74a306883492937de3051b6ac82263de
wohlfab <wohlfab@rose-hulman.edu>
2019-01-31 20:12:22 -0500

    Exam 3 Brandon Wohlfarth

--- src/problem4.py
+++ src/problem4.py
@@ -3,9 +3,9 @@ Exam 3, problem 4.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Brandon Wohlfarth.  January 2019.
          
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -17,7 +17,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the doc-string for the   is_prime   function defined below.
+# DONE: 2.  READ the doc-string for the   is_prime   function defined below.
 # It is the same as you have seen before.
 # After you UNDERSTAND the doc-string (JUST the doc-string, NOT the code),
 # ASKING QUESTIONS AS NEEDED, change the above _TODO_ to DONE.
@@ -275,7 +275,12 @@ def problem4(seq_of_seq, n):
     # TODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    for j in range(len(seq_of_seq)):
+        for k in range(len(seq_of_seq[j])):
+            if is_prime(seq_of_seq[j][k]) is True:
+                if seq_of_seq[j][k] > n:
+                    return seq_of_seq[j][k]
+    return -1
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
